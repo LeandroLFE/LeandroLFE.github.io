8734024b1bac15de2ccd40876d77e9ce71aa1d94
LeandroLFE <lfe.leandro@gmail.com>
2022-07-24 17:20:38 -0300

    correção do carregamento dos valores salvos

--- contador.js
+++ contador.js
@@ -145,9 +145,12 @@ function getConfig(){
         cor_fundo_resp.value = config.cor_de_fundo_resp
         espaco_resultado_interno.style.color = config.cor_da_fonte_resp
         cor_fonte_resp.value = config.cor_da_fonte_resp
-        espaco_resultado_interno.style.fontSize = config.tamanho_fonte_resp+"px"
+        espaco_resultado_interno.style.fontSize = config.tamanho_fonte_resp+" px"
         tamanho_fonte_resp_range.value = config.tamanho_fonte_resp
         tamanho_fonte_resp_num.value = config.tamanho_fonte_resp
+        espaco_resultado_interno.style.width = parseInt(tamanho_fonte_resp_num.value*1+2)*(qtde_formato_hora==3?8:6) + "px"  
+        resp_atual.style.fontSize = tamanho_fonte_resp_num.value + "px"
+        resp_total.style.fontSize = tamanho_fonte_resp_num.value + "px"
         chk_contador_automatico.checked = config.contador_automatico
         chk_apenas_minuto_segundo.checked = config.apenas_minuto_segundo
     }
@@ -164,146 +167,11 @@ function setConfig(){
     config.select_vel_video = select_vel_video.value;
     config.cor_de_fundo_resp = cor_fundo_resp.value;
     config.cor_da_fonte_resp = cor_fonte_resp.value;
-    config.tamanho_fonte_resp = espaco_resultado_interno.style.fontSize?espaco_resultado_interno.style.fontSize.replace("px", ""):tamanho_fonte_resp_num.value;
+    config.tamanho_fonte_resp = tamanho_fonte_resp_num.value;
     config.apenas_minuto_segundo = chk_apenas_minuto_segundo.checked;
     config.contador_automatico = chk_contador_automatico.checked;
     localStorage.setItem('config', JSON.stringify(config));
 }
-
-const select_vel_video = document.querySelector('#selectVelocidadeVideo');
-const hora_atual = document.querySelector('#hour-current'); 
-const separador_hora_atual = document.querySelector('#hour-current-separator'); 
-const min_atual = document.querySelector('#min-current'); 
-const seg_atual = document.querySelector('#sec-current');
-const hora_total = document.querySelector('#hour-amount'); 
-const separador_hora_total = document.querySelector('#hour-amount-separator'); 
-const min_total = document.querySelector('#min-amount'); 
-const seg_total = document.querySelector('#sec-amount');
-const elementos_tempo = document.getElementsByClassName('time');
-
-const resp_atual = document.querySelector('#resp-atual');
-const resp_total = document.querySelector('#resp-total');
-const espaco_resultado_interno = document.querySelector('.espaco-result-interno');
-
-const btn_play_pause = document.getElementById('btn-play-pause');
-const btn_reset = document.getElementById('btn-reset')
-const btn_update = document.getElementById('btn-update')
-const btn_save = document.getElementById('btn-save')
-const btn_options = document.getElementById('btn-options')
-
-const div_options = document.querySelector('#div-opcoes')
-let exibe_div_options = false
-div_options.style.display = "none"
-
-const div_info_automatico = document.querySelector('#info-automatico');
-const chk_contador_automatico = document.querySelector('#contador-automatico');
-const chk_apenas_minuto_segundo = document.querySelector('#apenas-minuto-segundo');
-const cor_fundo_resp = document.querySelector('#cor-fundo')
-const cor_fonte_resp = document.querySelector('#cor-fonte')
-const tamanho_fonte_resp_range = document.querySelector('#tamanho-fonte-range')
-const tamanho_fonte_resp_num  = document.querySelector('#tamanho-fonte-num')
-
-let cor_verde = '#00AA00';
-let cor_vermelha = '#DF0000';
-let cor_atual = cor_vermelha;
-let cor_hover = '#C0C0C0';
-
-let play_pause = false;
-let loop_execucao;
-let config = getConfig()
-
-let modo_contador_automatico = chk_contador_automatico.checked;
-if(modo_contador_automatico){
-    div_info_automatico.style.display="block"
-    // btn_play_pause.style.display="none"
-    // btn_reset.style.display="none"
-} else{
-    div_info_automatico.style.display="none"
-    // btn_play_pause.style.display="inline-block"
-    // btn_reset.style.display="inline-block"
-}
-
-let exibe_apenas_min_e_seg = chk_apenas_minuto_segundo.checked;
-
-tamanho_fonte_resp_num.value = tamanho_fonte_resp_range.value
-
-btn_play_pause.style.backgroundColor = cor_vermelha
-
-let vel_video = parseFloat(select_vel_video.value)
-
-let tempo_inicial = [hora_atual.value ? hora_atual.value : 0, 
-                    min_atual.value ? min_atual.value : 0,
-                    seg_atual.value ? seg_atual.value : 0];
-
-let tempo_atual_array = tempo_inicial;
-
-let tempo_total_array = [hora_total.value ? hora_total.value : 0, 
-                        min_total.value ? min_total.value : 0,
-                        seg_total.value ? seg_total.value : 0];
-
-let dateTime_tempo_atual = atribui_hora_min_seg(tempo_atual_array);
-let dateTime_tempo_total = atribui_hora_min_seg(tempo_total_array);
-
-let intervalo_loop = 1000.0 / vel_video;
-let diff_dates = (dateTime_tempo_total - dateTime_tempo_atual) / intervalo_loop;
-
-let qtde_formato_hora = 3;
-
-let tempo_atual = monta_hora_min_seg(dateTime_tempo_atual, qtde_formato_hora);
-let tempo_total = monta_hora_min_seg(dateTime_tempo_total, qtde_formato_hora);
-
-esconde_exibe_min_seg()
-
-resp_atual.innerHTML = tempo_atual;
-resp_total.innerHTML = tempo_total;
-
-select_vel_video.addEventListener('input', () => {
-    vel_video = parseFloat(select_vel_video.value)
-    intervalo_loop = 1000.0 / vel_video;
-    diff_dates = (dateTime_tempo_total - dateTime_tempo_atual) / intervalo_loop;
-    resp_atual.innerHTML = tempo_atual;
-    resp_total.innerHTML = tempo_total;
-})
-
-btn_play_pause.addEventListener("mouseenter", function( event ) {   
-    btn_play_pause.style.backgroundColor = cor_hover;
-  }, false);
-
-btn_play_pause.addEventListener("mouseleave", function( event ) {   
-    btn_play_pause.style.backgroundColor = cor_atual;
-  }, false);
-
-btn_play_pause.addEventListener('click', () => {
-    play_pause = !play_pause;
-    if(play_pause)
-        inicia_loop_execucao()
-})
-
-btn_reset.addEventListener('click', reset_contador)
-
-btn_update.addEventListener('click', ()=>{
-    tempo_atual_array = [hora_atual.value ? hora_atual.value : 0, 
-        min_atual.value ? min_atual.value : 0,
-        seg_atual.value ? seg_atual.value : 0];
-    tempo_total_array = [hora_total.value ? hora_total.value : 0, 
-        min_total.value ? min_total.value : 0,
-        seg_total.value ? seg_total.value : 0]; 
-    dateTime_tempo_atual = atribui_hora_min_seg(tempo_atual_array);
-    dateTime_tempo_total = atribui_hora_min_seg(tempo_total_array);
-    tempo_atual = monta_hora_min_seg(dateTime_tempo_atual, qtde_formato_hora);
-    tempo_total = monta_hora_min_seg(dateTime_tempo_total, qtde_formato_hora);
-    resp_atual.innerHTML = tempo_atual;
-    resp_total.innerHTML = tempo_total;
-})
-
-btn_options.addEventListener('click', ()=>{
-    exibe_div_options = !exibe_div_options
-    div_options.style.display = exibe_div_options? 'block' : 'none' 
-})
-
-btn_save.addEventListener('click', () => {
-    setConfig()
-})
 
 function testa_valor_caixa(element, limite){ 
     if(element.value<0){
@@ -315,51 +183,289 @@ function testa_valor_caixa(element, limite){
     element.value = String(element.value).length > 2 ? parseInt(element.value) : element.value
 }
 
-chk_contador_automatico.addEventListener('input', ()=>{
-    modo_contador_automatico = chk_contador_automatico.checked
-    if(modo_contador_automatico){
-        div_info_automatico.style.display="block"
-        // btn_play_pause.style.display="none"
-        // btn_reset.style.display="none"
-    } else{
-        div_info_automatico.style.display="none"
-        // btn_play_pause.style.display="inline-block"
-        // btn_reset.style.display="inline-block"
+let select_vel_video;
+let hora_atual; 
+let separador_hora_atual; 
+let min_atual; 
+let seg_atual;
+let hora_total; 
+let separador_hora_total; 
+let min_total; 
+let seg_total;
+let elementos_tempo;
+
+let resp_atual;
+let resp_total;
+let espaco_resultado_interno;
+
+let btn_play_pause;
+let btn_reset;
+let btn_update;
+let btn_save;
+let btn_options;
+
+let div_options;
+let exibe_div_options;
+
+let div_info_automatico;
+let chk_contador_automatico;
+let chk_apenas_minuto_segundo;
+let cor_fundo_resp;
+let cor_fonte_resp;
+let tamanho_fonte_resp_range;
+let tamanho_fonte_resp_num;
+
+let cor_verde = '#00AA00';
+let cor_vermelha = '#DF0000';
+let cor_atual = cor_vermelha;
+let cor_hover = '#C0C0C0';
+
+let play_pause = false;
+let loop_execucao;
+let config = {}
+
+let modo_contador_automatico;
+
+let exibe_apenas_min_e_seg;
+
+let vel_video;
+
+let tempo_inicial;
+
+let tempo_atual_array;
+
+let tempo_total_array;
+
+let dateTime_tempo_atual;
+let dateTime_tempo_total;
+
+let intervalo_loop;
+let diff_dates;
+
+let qtde_formato_hora;
+
+let tempo_atual;
+let tempo_total;
+
+function testa_valor_caixa(element, limite){ 
+    if(element.value<0){
+        element.value = 0;
     }
-    reset_contador()
-})
+    if(element.value>=limite){
+        element.value = limite-1;
+    }    
+    element.value = String(element.value).length > 2 ? parseInt(element.value) : element.value
+}
 
-chk_apenas_minuto_segundo.addEventListener('input', ()=>{
-    if(chk_apenas_minuto_segundo.checked){
-        exibe_apenas_min_e_seg = true
-    } else{
-        exibe_apenas_min_e_seg = false
+try{
+    window.onload = ()=>{
+        select_vel_video = document.querySelector('#selectVelocidadeVideo');
+        hora_atual = document.querySelector('#hour-current'); 
+        separador_hora_atual = document.querySelector('#hour-current-separator'); 
+        min_atual = document.querySelector('#min-current'); 
+        seg_atual = document.querySelector('#sec-current');
+        hora_total = document.querySelector('#hour-amount'); 
+        separador_hora_total = document.querySelector('#hour-amount-separator'); 
+        min_total = document.querySelector('#min-amount'); 
+        seg_total = document.querySelector('#sec-amount');
+        elementos_tempo = document.getElementsByClassName('time');
+
+        resp_atual = document.querySelector('#resp-atual');
+        resp_total = document.querySelector('#resp-total');
+        espaco_resultado_interno = document.querySelector('.espaco-result-interno');
+
+        btn_play_pause = document.getElementById('btn-play-pause');
+        btn_reset = document.getElementById('btn-reset')
+        btn_update = document.getElementById('btn-update')
+        btn_save = document.getElementById('btn-save')
+        btn_options = document.getElementById('btn-options')
+
+        div_options = document.querySelector('#div-opcoes')
+        exibe_div_options = false
+        div_options.style.display = "none"
+
+        div_info_automatico = document.querySelector('#info-automatico');
+        chk_contador_automatico = document.querySelector('#contador-automatico');
+        chk_apenas_minuto_segundo = document.querySelector('#apenas-minuto-segundo');
+        cor_fundo_resp = document.querySelector('#cor-fundo')
+        cor_fonte_resp = document.querySelector('#cor-fonte')
+        tamanho_fonte_resp_range = document.querySelector('#tamanho-fonte-range')
+        tamanho_fonte_resp_num  = document.querySelector('#tamanho-fonte-num')
+
+        cor_verde = '#00AA00';
+        cor_vermelha = '#DF0000';
+        cor_atual = cor_vermelha;
+        cor_hover = '#C0C0C0';
+
+        play_pause = false;
+        config = {}
+        config=getConfig()
+
+        modo_contador_automatico = chk_contador_automatico.checked;
+        if(modo_contador_automatico){
+            div_info_automatico.style.display="block"
+            // btn_play_pause.style.display="none"
+            // btn_reset.style.display="none"
+        } else{
+            div_info_automatico.style.display="none"
+            // btn_play_pause.style.display="inline-block"
+            // btn_reset.style.display="inline-block"
+        }
+
+        exibe_apenas_min_e_seg = chk_apenas_minuto_segundo.checked;
+
+        tamanho_fonte_resp_num.value = tamanho_fonte_resp_range.value
+
+        btn_play_pause.style.backgroundColor = cor_vermelha
+
+        vel_video = parseFloat(select_vel_video.value)
+
+        tempo_inicial = [hora_atual.value ? hora_atual.value : 0, 
+                            min_atual.value ? min_atual.value : 0,
+                            seg_atual.value ? seg_atual.value : 0];
+
+        tempo_atual_array = tempo_inicial;
+
+        tempo_total_array = [hora_total.value ? hora_total.value : 0, 
+                                min_total.value ? min_total.value : 0,
+                                seg_total.value ? seg_total.value : 0];
+
+        dateTime_tempo_atual = atribui_hora_min_seg(tempo_atual_array);
+        dateTime_tempo_total = atribui_hora_min_seg(tempo_total_array);
+
+        intervalo_loop = 1000.0 / vel_video;
+        diff_dates = (dateTime_tempo_total - dateTime_tempo_atual) / intervalo_loop;
+
+        qtde_formato_hora = 3;
+
+        tempo_atual = monta_hora_min_seg(dateTime_tempo_atual, qtde_formato_hora);
+        tempo_total = monta_hora_min_seg(dateTime_tempo_total, qtde_formato_hora);
+
+        esconde_exibe_min_seg()
+
+        resp_atual.innerHTML = tempo_atual;
+        resp_total.innerHTML = tempo_total;
+
+        select_vel_video.addEventListener('input', () => {
+            vel_video = parseFloat(select_vel_video.value)
+            intervalo_loop = 1000.0 / vel_video;
+            diff_dates = (dateTime_tempo_total - dateTime_tempo_atual) / intervalo_loop;
+            resp_atual.innerHTML = tempo_atual;
+            resp_total.innerHTML = tempo_total;
+        })
+        
+        btn_play_pause.addEventListener("mouseenter", function( event ) {   
+            btn_play_pause.style.backgroundColor = cor_hover;
+        }, false);
+        
+        btn_play_pause.addEventListener("mouseleave", function( event ) {   
+            btn_play_pause.style.backgroundColor = cor_atual;
+        }, false);
+        
+        btn_play_pause.addEventListener('click', () => {
+            play_pause = !play_pause;
+            if(play_pause)
+                inicia_loop_execucao()
+        })
+        
+        btn_reset.addEventListener('click', reset_contador)
+        
+        btn_update.addEventListener('click', ()=>{
+            tempo_atual_array = [hora_atual.value ? hora_atual.value : 0, 
+                min_atual.value ? min_atual.value : 0,
+                seg_atual.value ? seg_atual.value : 0];
+            tempo_total_array = [hora_total.value ? hora_total.value : 0, 
+                min_total.value ? min_total.value : 0,
+                seg_total.value ? seg_total.value : 0]; 
+            dateTime_tempo_atual = atribui_hora_min_seg(tempo_atual_array);
+            dateTime_tempo_total = atribui_hora_min_seg(tempo_total_array);
+            tempo_atual = monta_hora_min_seg(dateTime_tempo_atual, qtde_formato_hora);
+            tempo_total = monta_hora_min_seg(dateTime_tempo_total, qtde_formato_hora);
+            resp_atual.innerHTML = tempo_atual;
+            resp_total.innerHTML = tempo_total;
+        })
+        
+        btn_options.addEventListener('click', ()=>{
+            exibe_div_options = !exibe_div_options
+            div_options.style.display = exibe_div_options? 'block' : 'none' 
+        })
+        
+        btn_save.addEventListener('click', () => {
+            setConfig()
+        })
+        
+        hora_atual.addEventListener("change", ()=>{
+            testa_valor_caixa(this, 24)
+        });
+        
+        min_atual.addEventListener("change", ()=>{
+            testa_valor_caixa(this, 60)
+        })
+        
+        seg_atual.addEventListener("change", ()=>{
+            testa_valor_caixa(this, 60)
+        })
+        
+        hora_total.addEventListener("change", ()=>{
+            testa_valor_caixa(this, 24)
+        });
+        
+        min_total.addEventListener("change", ()=>{
+            testa_valor_caixa(this, 60)
+        })
+        
+        seg_total.addEventListener("change", ()=>{
+            testa_valor_caixa(this, 60)
+        })
+
+        chk_apenas_minuto_segundo.addEventListener('input', ()=>{
+            if(chk_apenas_minuto_segundo.checked){
+                exibe_apenas_min_e_seg = true
+            } else{
+                exibe_apenas_min_e_seg = false
+            }
+            esconde_exibe_min_seg()
+        })
+        
+        chk_contador_automatico.addEventListener('input', ()=>{
+            modo_contador_automatico = chk_contador_automatico.checked
+            if(modo_contador_automatico){
+                div_info_automatico.style.display="block"
+                // btn_play_pause.style.display="none"
+                // btn_reset.style.display="none"
+            } else{
+                div_info_automatico.style.display="none"
+                // btn_play_pause.style.display="inline-block"
+                // btn_reset.style.display="inline-block"
+            }
+            reset_contador()
+        })
+
+        cor_fundo_resp.addEventListener('input', ()=>{
+            espaco_resultado_interno.style.backgroundColor = cor_fundo_resp.value;
+        })
+        
+        cor_fonte_resp.addEventListener('input', () =>{
+            espaco_resultado_interno.style.color = cor_fonte_resp.value;
+        })
+        
+        tamanho_fonte_resp_range.addEventListener('input', ()=>{    
+            tamanho_fonte_resp_num.value = tamanho_fonte_resp_range.value
+            espaco_resultado_interno.style.fontSize = tamanho_fonte_resp_num.value + "px"
+            espaco_resultado_interno.style.width = parseInt(tamanho_fonte_resp_num.value*1+2)*(qtde_formato_hora==3?8:6) + "px"  
+            resp_atual.style.fontSize = tamanho_fonte_resp_num.value + "px"
+            resp_total.style.fontSize = tamanho_fonte_resp_num.value + "px"
+        
+        })
+        
+        tamanho_fonte_resp_num.addEventListener('input', () => {
+            espaco_resultado_interno.style.width = parseInt(tamanho_fonte_resp_num.value*1+2)*(qtde_formato_hora==3?8:6) + "px" 
+            tamanho_fonte_resp_range.value = tamanho_fonte_resp_num.value
+            resp_atual.style.fontSize = tamanho_fonte_resp_num.value + "px"
+            resp_total.style.fontSize = tamanho_fonte_resp_num.value + "px"
+        })
+        
     }
-    esconde_exibe_min_seg()
-})
-
-cor_fundo_resp.addEventListener('input', ()=>{
-    espaco_resultado_interno.style.backgroundColor = cor_fundo_resp.value;
-})
-
-cor_fonte_resp.addEventListener('input', () =>{
-    espaco_resultado_interno.style.color = cor_fonte_resp.value;
-})
-
-tamanho_fonte_resp_range.addEventListener('input', ()=>{    
-    tamanho_fonte_resp_num.value = tamanho_fonte_resp_range.value
-    espaco_resultado_interno.style.fontSize = tamanho_fonte_resp_num.value + "px"
-    espaco_resultado_interno.style.width = parseInt(tamanho_fonte_resp_num.value*1+2)*(qtde_formato_hora==3?8:6) + "px"  
-    resp_atual.style.fontSize = tamanho_fonte_resp_num.value + "px"
-    resp_total.style.fontSize = tamanho_fonte_resp_num.value + "px"
-
-})
-
-tamanho_fonte_resp_num.addEventListener('input', () => {
-    espaco_resultado_interno.style.width = parseInt(tamanho_fonte_resp_num.value*1+2)*(qtde_formato_hora==3?8:6) + "px" 
-    tamanho_fonte_resp_range.value = tamanho_fonte_resp_num.value
-    resp_atual.style.fontSize = tamanho_fonte_resp_num.value + "px"
-    resp_total.style.fontSize = tamanho_fonte_resp_num.value + "px"
-
-})
-
+} catch(err){
+    console.log(err);
+}
